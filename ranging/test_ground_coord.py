90b7d3db23ee45e0897822c7682c22348473f217
FLAGKING <1900241804@qq.com>
2023-03-15 13:13:17 +0800

    测试坐标计算模块

--- ranging/test_ground_coord.py
+++ ranging/test_ground_coord.py
@@ -11,71 +11,88 @@ def grid_to_tv(
         tv_origin_y=-1500,
         tv_width=7500,
         tv_height=11000):
-    tv_x = ((pos % grid_width) + 0.5) * (tv_width / grid_width) + tv_origin_x
-    tv_y = ((pos / grid_width) + 0.5) * (tv_height / grid_height) + tv_origin_y
+    if pos < 0:
+        return [-1, -1]
+    tv_x = int((pos % grid_width) + 0.5) * (tv_width / grid_width) + tv_origin_x
+    tv_y = int((pos / grid_width) + 0.5) * (tv_height / grid_height) + tv_origin_y
 
-    return [tv_x, tv_y, 1]
+    return np.array([tv_x, tv_y])
+
+
+def ground_coord(pos_file, camera_file, save_path):
+    import csv
+
+    pos = np.loadtxt(pos_file, skiprows=2)
+    cap = cv2.VideoCapture(camera_file)
+    predictor = create_predictor(exp_name='yolox-s', conf=0.4, nms=0.6, device='gpu')
+
+    with open('{}/gt_terrace1.csv'.format(save_path), 'w', encoding='utf-8', newline='') as csv_gt:
+        with open('{}/gv_terrace1.csv'.format(save_path), 'w', encoding='utf-8', newline='') as csv_gv:
+            writer_gt = csv.writer(csv_gt, delimiter=',')
+            writer_gv = csv.writer(csv_gv)
+
+            for row in range(len(pos)):
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                if row % 25 != 0 or row == 0:
+                    continue
+
+                truth = np.empty(shape=[0, 2])
+                for col in range(len(pos[row])):
+                    rv = grid_to_tv(pos[row][col])
+                    truth = np.append(truth, [rv], axis=0).tolist()
+                writer_gt.writerow([n for n in truth])
+
+                boxes, scores = get_boxes(predictor, frame)
+                coord = get_ground_coord(f=19.9, dx=0.023, dy=0.023, h=2000, alpha=0.28,
+                                         u0=355 / 2, v0=241 / 2, boxes=boxes)
+                if coord:
+                    coord = np.round(coord).tolist()
+                    writer_gv.writerow([n for n in coord])
+                else:
+                    writer_gv.writerow('')
 
 
 if __name__ == '__main__':
-    pos = np.loadtxt('../assets/gt_terrace1.txt', skiprows=2)
-    truth = np.array([])
-    for i in range(6, 10):
-        truth = np.append(truth, grid_to_tv(pos[i * 25][0]))
-
-    test_truth = np.array([])
-    for i in range(10, 14):
-        test_truth = np.append(test_truth, grid_to_tv(pos[i * 25][0]))
-
-    # 读取视频
-    cap = cv2.VideoCapture('../assets/terrace1-c0.avi')
-
-    # 生成预测器
-    predictor = create_predictor(exp_name='yolox-s', conf=0.4, nms=0.6, device='gpu')
-
-    count = 1
-    coord = np.array([])
-    test_coord = np.array([])
-    while True:
-        # 读取一帧图像
-        ret, frame = cap.read()
-
-        # 获取人检测框与置信度集合
-        boxes, scores = get_boxes(predictor, frame)
-        cls_ids = [0 for n in range(len(boxes))]
-
-        if 225 >= count >= 150 and count % 25 == 0:
-            temp = get_ground_coord(f=19.9, dx=0.023, dy=0.023, h=2000, alpha=0.22,
-                                    u0=355 / 2, v0=241 / 2, boxes=boxes)[0]
-            temp.append(1)
-            coord = np.append(coord, temp)
-
-        if 325 >= count >= 250 and count % 25 == 0:
-            temp = get_ground_coord(f=19.9, dx=0.023, dy=0.023, h=2000, alpha=0.22,
-                                    u0=355 / 2, v0=241 / 2, boxes=boxes)[0]
-            temp.append(1)
-            test_coord = np.append(test_coord, temp)
-
-        if count > 325:
-            break
-        count += 1
-
-    truth = truth.reshape(4, 3)
-    test_truth = test_truth.reshape(4, 3)
-
-    coord = coord.reshape(4, 3)
-    test_coord = test_coord.reshape(4, 3)
-
     import os
-
     os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-    # 求解变换矩阵 H
-    H = np.linalg.solve(coord.T.dot(coord), coord.T.dot(truth))
 
-    print(truth)
-    print(coord)
-    # print(np.dot(coord, H))
-    # print(H)
-    print(test_truth)
-    print(test_truth)
-    # print(np.dot(test_coord, H))
+    # pos_file = '../assets/gt_terrace1.txt'
+    # camera_file = '../assets/terrace1-c2.avi'
+    # outputs_file = 'outputs'
+    # ground_coord(pos_file, camera_file, outputs_file)
+
+    import csv
+    with open('outputs/gt_terrace1.csv', 'r', encoding='utf-8') as csv_gt:
+        with open('outputs/gv_terrace1.csv', 'U', encoding='utf-8') as csv_gv:
+            reader_gt = csv.reader(csv_gt)
+            reader_gv = csv.reader(csv_gv)
+
+            gt = np.empty(shape=[0, 2])
+            count = 0
+
+            for row in csv_gt:
+                arr_row = [eval(n) for n in eval(row)]
+                if arr_row[0][0] != -1:
+                    gt = np.append(gt, [arr_row[0]], axis=0)
+                    count += 1
+                if count == 10:
+                    break
+            print(gt)
+
+            gv = np.empty(shape=[0, 2])
+            for row in csv_gv:
+                if len(row) > 1:
+                    a=eval(row)
+                    b=eval(a)
+                    arr_row = [eval(n) for n in eval(row)]
+                    gt = np.append(gt, arr_row[0], axis=0)
+            print(gv)
+
+
+
+    # 求解变换矩阵 H
+    # H = np.linalg.solve(coord.T.dot(coord), coord.T.dot(truth))
+
+
